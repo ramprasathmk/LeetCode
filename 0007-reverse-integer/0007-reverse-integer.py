b65e996b48caac0a288558c3c0dab5a616983c82
Ramprasath M K <144304719+ramprasathmk@users.noreply.github.com>
2024-09-18 12:16:44 +0545

    Time: 43 ms (13.82%), Space: 16.6 MB (5.01%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,17 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:
 
+        if not isinstance(x, int):
+            raise Exception(f"Invalid user type for variable `x`")
+
         INT_MIN: int = -2**31
         INT_MAX: int = 2**31 - 1
 
         result: int = 0
 
         sign: int = -1 if x<0 else 1
-        x = abs(x)
+        x: int = abs(x)
 
         while x != 0:
             digit = x % 10
-            x = x// 10
+            x = x // 10
 
             if result > (INT_MAX - digit) // 10:
                 return 0
