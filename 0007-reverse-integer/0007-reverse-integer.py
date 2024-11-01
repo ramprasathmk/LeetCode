4f1aa21a57a05c913681c3222b6b8993b648d100
Ramprasath M K <144304719+ramprasathmk@users.noreply.github.com>
2024-11-01 23:36:34 +0530

    Time: 34 ms (70.28%), Space: 16.6 MB (17.33%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -2,13 +2,10 @@ class Solution:
     def reverse(self, x: int) -> int:
 
         if not isinstance(x, int):
-            raise Exception(f"Invalid Type, {x=} must be an Integer.")
-        
+            raise Exception(f"Invalid user type for variable `x`")
+
         INT_MIN: int = -2**31
         INT_MAX: int = 2**31 - 1
-        
-        if x not in range(INT_MIN, INT_MAX+1):
-            raise Exception(f"{x=} is not in [ {INT_MIN} <= x <= {INT_MAX} ]")
 
         result: int = 0
 
@@ -26,4 +23,3 @@ class Solution:
 
         return sign * result        
     
-        
