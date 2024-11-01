b3b59e6bd5af986975734e64cce313c8c2056f51
Ramprasath M K <144304719+ramprasathmk@users.noreply.github.com>
2024-11-01 23:31:40 +0530

    Time: 0 ms (100%), Space: 16.5 MB (47.6%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
@@ -1,4 +1,14 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+
+        if not isinstance(target, int):
+            raise Exception(f"{target=} must be an Integer")
+
+        if not isinstance(hours, List):
+            raise Exception(f"{hours=} must be a Type: `List`")
+
+        if not isinstance(hours, list):
+            raise Exception(f"{hours=} must be a Type: `list`")
+
         return sum(num >= target for num in hours)
         
