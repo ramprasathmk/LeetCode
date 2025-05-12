97f49159b4e413ae2307aed56e1a6a3c16c534d1
Ramprasath M K <144304719+ramprasathmk@users.noreply.github.com>
2025-05-12 20:51:20 +0545

    Time: 0 ms (100%), Space: 3.9 MB (26.83%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.go
@@ -0,0 +1,16 @@
+func fib(n int) int {
+    if n == 0 {
+		return 0
+	}
+
+	// n1 and n2 are the (i-1)th and ith Fibonacci numbers, respectively
+	var n1, n2 int = 0, 1
+
+	for i := 1; i < n; i++ {
+		n3 := n1 + n2
+		n1 = n2
+		n2 = n3
+	}
+
+	return n2
+}
